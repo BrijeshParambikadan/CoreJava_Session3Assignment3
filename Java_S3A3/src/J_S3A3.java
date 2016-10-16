71e78d4b29a1c69a7d574802d6fc822c204772ef
BrijeshParambikadan <bbavittom@gmail.com>
2016-10-16 12:01:24 +0530

    submitted JavaS3A3 assignment

---
+++ Java_S3A3/src/J_S3A3.java
@@ -0,0 +1,18 @@
+
+ class J_S3A3 {
+
+	 int x; int y;
+			 
+	 String name;
+			 
+	 public static void main(String args[]) {
+	 J_S3A3 pnt = new J_S3A3();
+	 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+}
+}
+
+ 
+ //  Output will be d. The program prints pnt is null 0 0.
+ 
+ 
+ // Thank You
